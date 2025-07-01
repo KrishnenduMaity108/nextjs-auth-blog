39697c9abc33a7e58915c01c251fcc70b6dc0aae
Krish <krishnendumaity811.com>
2025-07-02 01:46:04 +0530

    Fix: blog/[slug]/page.tsx'

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,18 @@
 interface BlogPageProps {
-  params: { slug: string };
+  params: {
+    slug: string;
+  };
 }
 
-export default async function BlogPage({ params }: BlogPageProps) {
-   const { slug } = await params
+export default function BlogPage({ params }: BlogPageProps) {
+  const { slug } = params;
+
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-4">Blog Post</h1>
-      <p>Slug: <strong>{slug}</strong></p>
+      <p>
+        Slug: <strong>{slug}</strong>
+      </p>
     </div>
   );
 }
